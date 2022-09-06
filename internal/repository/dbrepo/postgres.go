b096dd717734649da8ca582e4552177fbf100749
Ray Krishardi Layadi <raykrishardi@gmail.com>
2022-09-06 20:58:47 +0700

    Section 13: Connecting our Application to the Database 107. Searching for availability by room

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -65,3 +65,29 @@ func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
 
 	return nil
 }
+
+func (m *postgresDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var numRows int
+
+	query := `
+											SELECT count(id)
+											FROM room_restrictions
+											WHERE room_id = $1
+											AND $2 < end_date AND $3 > start_date
+	`
+
+	row := m.DB.QueryRowContext(ctx, query, roomID, start, end)
+	err := row.Scan(&numRows)
+	if err != nil {
+		return false, err
+	}
+
+	if numRows == 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,9 +1,14 @@
 package repository
 
-import "github.com/tsawler/bookings-app/internal/models"
+import (
+	"time"
+
+	"github.com/tsawler/bookings-app/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
 }
