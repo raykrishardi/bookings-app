2297f11abc84e57bd154bf772cf27b8a42421c64
Ray Krishardi Layadi <raykrishardi@gmail.com>
2022-09-10 10:15:21 +0700

    Section 13: Connecting our Application to the Database 108. Searching for availability for all rooms

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -79,6 +79,7 @@ func (m *postgresDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time,
 											AND $2 < end_date AND $3 > start_date
 	`
 
+	// Querying 1 row, use QueryRowContext function
 	row := m.DB.QueryRowContext(ctx, query, roomID, start, end)
 	err := row.Scan(&numRows)
 	if err != nil {
@@ -90,4 +91,38 @@ func (m *postgresDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time,
 	}
 
 	return false, nil
+}
+
+func (m *postgresDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `
+	select r.id, r.room_name
+	from rooms r
+	where r.id not in (select rr.room_id from room_restrictions rr where $1 < rr.end_date and $2 > rr.start_date)
+	`
+
+	var rooms []models.Room
+
+	// Querying more than 1 row, use QueryContext function
+	rows, err := m.DB.QueryContext(ctx, query, start, end)
+	if err != nil {
+		return rooms, err
+	}
+
+	for rows.Next() {
+		var room models.Room
+		err := rows.Scan(&room.ID, &room.RoomName)
+		if err != nil {
+			return rooms, err
+		}
+		rooms = append(rooms, room)
+	}
+
+	if err = rows.Err(); err != nil {
+		return rooms, err
+	}
+
+	return rooms, nil
 }
