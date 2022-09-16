7fbe7b09bc68d14a1078fd3a433fe2c7be5c9250
Ray Krishardi Layadi <raykrishardi@gmail.com>
2022-09-16 23:26:39 +0700

    Section 16: Authentication 136. Creating the authentication and user database functions

---
+++ internal/repository/dbrepo/test-repo.go
@@ -0,0 +1,15 @@
+package dbrepo
+
+// func (m *testDBRepo) GetUserByID(id int) (models.User, error) {
+// 	var u models.User
+
+// 	return nil
+// }
+
+// func (m *testDBRepo) UpdateUser(u models.User) error {
+// 	return nil
+// }
+
+// func (m *testDBRepo) Authenticate(email, testPassword string) (int, string, error) {
+// 	return 1, "", nil
+// }
