091295c8ad00d65d778aaa4b84fb52544e6f8638
Ray Krishardi Layadi <raykrishardi@gmail.com>
2022-09-17 21:45:38 +0700

    Section 19: Deploying our application to a server 171. Add admin user to migrations

---
+++ migrations/20220917144302_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+	 ('ray','lay','admin@admin.com','$2a$12$XESHKPv/WGxRGZdrcxStZeDBr5eURVp6vY/OktGaERaZ3uByRFltu',3,'2022-09-17 00:00:00','2022-09-17 00:00:00');
